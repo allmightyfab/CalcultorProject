befb508582ad0b77c057dc05bd0c0b6bd1fbc7e5
allmightyfab <151810543+allmightyfab@users.noreply.github.com>
2024-09-22 21:48:09 -0500

    Model

---
+++ Models/CalcModel.cs
@@ -0,0 +1,28 @@
+﻿namespace CalcultorProject.Models
+{
+    public class CalcModel
+    {
+        public double num1 {get; set;}
+        public double num2 { get; set;}
+        public string addingthing { get; set;}
+        public double result { get; set;}
+
+        public double Calculate()
+        {
+            switch (addingthing)
+            {
+                case "+":
+                    return num1+num2;
+                case "-":
+                    return num1-num2;
+                case "*":
+                    return num1*num2;
+                case "/": 
+                    return num1/num2;
+
+            }
+        }
+
+
+    }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CalcultorProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
