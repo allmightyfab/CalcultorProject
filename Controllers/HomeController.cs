24c4be76cf13e01355fa549917ab4705db76a7af
allmightyfab <151810543+allmightyfab@users.noreply.github.com>
2024-09-22 21:57:28 -0500

    html try 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,23 @@
 using CalcultorProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Diagnostics;
 
 namespace CalcultorProject.Controllers
 {
     public class HomeController : Controller
     {
-        
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(CalcModel model)
+        {
+            
+        }
        
     }
 }
