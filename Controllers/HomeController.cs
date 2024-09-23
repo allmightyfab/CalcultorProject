c3f8e55c6a78ea3b639c899139a7ea238d2f7c88
allmightyfab <151810543+allmightyfab@users.noreply.github.com>
2024-09-22 21:34:26 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using CalcultorProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalcultorProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
